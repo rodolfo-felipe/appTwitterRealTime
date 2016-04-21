bd8f6e7db26e9ee9d7c8f21dd4ee06e2e64a5222
rodolfo-felipe <dolfinbh@gmail.com>
2016-04-20 21:43:30 -0300

    Correção de refactory
    Correção de refactory de limpeza de última consulta.

--- public/js/socketTweet.js
+++ public/js/socketTweet.js
@@ -6,7 +6,7 @@ $('form').submit(function(){
 	$('#txtTermoBuscado').append($('<b>').text(sTermo));
 	socket.emit('buscar', sTermo);
 
-	$('#tweets').empty();
+	$('#lista_tweets').empty();
 	socket.on('tweetCapturado', function(tweet){
 		var txtTweet = '<li class="lead h6"><b>Nome:</b> ' + tweet.user.name + '</br>' + '<b>Tweet:</b> ' + tweet.text + '</li>';
 		$('#lista_tweets').append(txtTweet);
